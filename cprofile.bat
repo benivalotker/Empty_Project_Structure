6de0338f389364e11fd59e83c4b749fd065001c5
Beni Valotker <BeniV@traffilog.com>
2020-06-30 09:51:31 +0300

    change profiler pacakge

--- cprofile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-python -B -m cProfile -o ./profile/profile_output.out  corrupted_stream.py
-pyprof2calltree -i ./profile/profile_output.out -o ./profile/prof.calltree
-
---
+++ pyinstrument.bat
@@ -0,0 +1,2 @@
+@echo off
+pyinstrument --outfile="./profile/profile.html" --renderer="html"  .\main_project.py
